b3659563f741396ca0f3e6c466de2d53543c66c4
Ratchagarn <ratchagarn@gmail.com>
2014-10-06 23:14:46 +0700

    Release version 0.0.2

--- README.md
+++ README.md
@@ -3,12 +3,51 @@ Jedo
 
 # Simple Javascript template component
 
-### Current Version: 0.0.1
+### Current Version: 0.0.2
 
 
 ## Change log
 
 ### 0.0.1
 - Init project
+- Add callback for `update` method
 
 
+## Example
+
+```
+var Test = Jedo.createUI({
+
+  afterRender: function() {
+    this.$node.on('submit', function(e) {
+      e.preventDefault();
+      this.update({
+        start_text: this.$node.find('input').val()
+      });
+
+    }.bind(this));
+  },
+
+
+  afterUpdate: function() {
+    this.$node.find('input').focus();
+  },
+
+  template: function() {
+
+    return (
+      '<div class="example=ui">' +
+        '<form>' +
+          '<input type="text" />' +
+          '<button type="submit">Submit</button>' +
+        '</form>' +
+        '<p class="output"><%= start_text %></p>' +
+      '</div>'
+    );
+
+  }
+
+});
+
+Test.render(document.body, { start_text: 'Hello world' });
+```
--- dist/jedo.js
+++ dist/jedo.js
@@ -141,25 +141,28 @@ var Jedo = {
        * @name render
        * @param {String} jQuery selector for mountnode
        * @param {Object} UI data
+       * @param {Function} Callback after render
        */
       
-      render: function(selector, data) {
+      render: function(selector, data, callback) {
 
         scope = $.extend({}, this, setting);
-        scope.data = data;
+        scope.$data = data;
 
         if (typeof setting.init === 'function') {
           setting.init.call(scope);
           setting.init = noop();
         }
 
-        var tpl = tmpl(setting.template(), data),
+        var tpl = tmpl(setting.template.call(scope), data),
             $mountNode = $(selector);
         $mountNode.html( tpl );
 
         setTimeout(function() {
 
           scope.$node = $mountNode;
+
+          (callback || noop)();
 
           if (typeof setting.afterRender === 'function') {
             setting.afterRender.call(scope);
@@ -179,11 +182,13 @@ var Jedo = {
        * ------------------------------------------------------------
        * @name name
        * @param {Object} UI data for update
+       * @param {Function} Callback after render
        */
       
-      update: function(data) {
-        scope.data = $.extend({}, scope.data, data);
-        scope.render(scope.$node, scope.data);
+      update: function(data, callback) {
+        if (data == null) { data = {}; }
+        scope.$data = $.extend({}, scope.$data, data);
+        scope.render(scope.$node, scope.$data, callback);
       }
 
     };
