11ba6d2df1f2b9ec6a4fbaa741d06cc0fa391646
Ratchagarn <ratchagarn@gmail.com>
2014-10-07 19:45:35 +0700

    Update new method

--- src/jedo.js
+++ src/jedo.js
@@ -4,9 +4,19 @@
  * ------------------------------------------------------------
  */
 
+
 (function(tmpl, $) {
 
 'use strict';
+
+/**
+ * ------------------------------------------------------------
+ * Global
+ * ------------------------------------------------------------
+ */
+
+var ID = 0;
+
 
 
 /**
@@ -18,6 +28,50 @@
 function noop() {}
 
 
+/**
+ * Generate UI ID
+ * ------------------------------------------------------------
+ * @name getID
+ * @return {String} ui string ID
+ */
+
+function getID() {
+  return ++ID;
+}
+
+
+/**
+ * Render template
+ * ------------------------------------------------------------
+ * @name _render
+ * @param {Function} template output function
+ * @param {Object} template mount node
+ * @param {Function} callback after template is mount
+ */
+
+function _render(scope, callback) {
+  var tpl = tmpl(scope.template.call(scope), scope.$data),
+            result = {};
+
+  if (scope.$node.length) {
+    scope.$node.html(tpl).attr('data-jedo-id', getID());
+  }
+  else {
+    var id = getID();
+    result = {
+      tpl: '<span data-jedo-id=' + id + '>' + tpl + '</span>',
+      $node: '[data-jedo-id=' + id + ']'
+    };
+  }
+
+  setTimeout(function() {
+    (callback || noop).call(scope);
+  });
+
+  return result;
+}
+
+
 var Jedo = {
 
   /**
@@ -26,9 +80,11 @@ var Jedo = {
    * ------------------------------------------------------------
    */
   
-  createUI: function(setting) {
+  createUI: function(settings) {
 
+    // scope variable for sharing context
     var scope = {};
+
 
     /**
      * ------------------------------------------------------------
@@ -50,34 +106,51 @@ var Jedo = {
       
       render: function(selector, data, callback) {
 
-        scope = $.extend({}, this, setting);
+        // assign this scope
+        scope = $.extend({}, this, settings);
         scope.$data = data;
+        scope.$node = $(selector);
 
-        if (typeof setting.init === 'function') {
-          setting.init.call(scope);
-          setting.init = noop();
-        }
+        (settings.init || noop).call(scope);
 
-        var tpl = tmpl(setting.template.call(scope), data),
-            $mountNode = $(selector);
-        $mountNode.html( tpl );
-
-        setTimeout(function() {
-
-          scope.$node = $mountNode;
-
-          (callback || noop)();
-
-          if (typeof setting.afterRender === 'function') {
-            setting.afterRender.call(scope);
-            setting.afterRender = noop();
+        var result = _render(scope, function() {
+          (callback || noop).call(scope);
+          (settings.afterRender || noop).call(scope);
+          if (result.$node) {
+            scope.$node = $(result.$node);
           }
-          else {
-            (setting.afterUpdate || noop).call(scope);
-          }
-
         });
 
+        if (result.tpl) { return result.tpl; }
+      },
+
+
+      /**
+       * Render template to HTML
+       * ------------------------------------------------------------
+       * @name toHTML
+       * @param {Object} UI data for template
+       * @return {String} HTML of template
+       */
+      
+      toHTML: function(data, callback) {
+        if (data == null) { data = {}; }
+        return this.render(undefined, data, callback);
+      },
+
+
+      /**
+       * Alias of Microtemplates
+       * ------------------------------------------------------------
+       * @name compile
+       * @param {String} template string
+       * @param {Object} data for template
+       * @return {String} string of template
+       */
+
+      compile: function(str, data) {
+        if (data == null) { data = {}; }
+        return tmpl(str, data);
       },
 
 
@@ -92,7 +165,10 @@ var Jedo = {
       update: function(data, callback) {
         if (data == null) { data = {}; }
         scope.$data = $.extend({}, scope.$data, data);
-        scope.render(scope.$node, scope.$data, callback);
+        _render(scope, function() {
+          (callback || noop).call(scope);
+          (settings.afterRenderUpdate || noop).call(scope);
+        });
       }
 
     };
@@ -103,7 +179,6 @@ var Jedo = {
 
 
 window.Jedo = Jedo;
-window.Jedo.map = jQuery.map;
 
 
-}).call(this, tmpl, jQuery);
+}).call(this, microtemplate, jQuery);
